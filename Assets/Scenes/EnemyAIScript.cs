e9d6438dabeba4a517dd2c3a573d04bb3e83aa69
Simon <simon2116@hotmail.com>
2022-03-05 15:08:24 -0500

    Adding scenes

---
+++ Assets/Scenes/EnemyAIScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIScript : MonoBehaviour
+{
+[SerializeField] Transform target;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+ 
+
+
+
+
+
+}
